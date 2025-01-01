3c5f4083de26a7e588fc3f76e426bfab6a0889af
kelbelss <124790103+kelbelss@users.noreply.github.com>
2025-01-01 15:47:04 +0200

    set up

---
+++ script/ChronoLockScript.s.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.28;
+
+import {Script, console} from "forge-std/Script.sol";
+import {ChronoLock} from "../src/ChronoLock.sol";
+
+contract ChronoLockScript is Script {}
---
+++ test/ChronoLockTest.t.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.28;
+
+import {Test, console} from "forge-std/Test.sol";
+import {ChronoLock} from "../src/ChronoLock.sol";
+
+contract ChronoLockTest is Test {}
