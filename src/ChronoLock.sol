614afaf6ea28f180a8986b3d0f2d5236439d397c
kelbelss <124790103+kelbelss@users.noreply.github.com>
2025-01-10 09:19:05 +0200

    set up basic variables and structs

--- src/ChronoLock.sol
+++ src/ChronoLock.sol
@@ -1,4 +1,31 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.28;
 
-contract ChronoLock {}
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract ChronoLock {
+    // state variables
+
+    // use multisig for owner
+    address public owner;
+
+    // Mapping - store vesting schedules: Company Address => (Beneficiary Address => VestingSchedule)
+    mapping(address company => mapping(address beneficiary => VestingSchedule)) public vestingSchedules;
+
+    // structs
+
+    // vesting schedule for a beneficiary.
+    struct VestingSchedule {
+        address token; // Address of company ERC20
+        address beneficiary; // Address of beneficiary
+        uint256 totalAmount; // Total amount of tokens to be vested
+        uint256 startTime; // block.timestamp when vesting begins
+        uint256 duration; // Duration of the vesting period (seconds)
+        uint256 cliffDuration; // Duration of the cliff period (seconds)
+        uint256 claimedAmount; // Amount of tokens already claimed
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+}
